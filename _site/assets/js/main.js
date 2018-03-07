e6053303a5ec3a6840b2cc1b10aea1c651280fd7
Sarah <sezfenton@icloud.com>
2018-03-08 05:39:43 +0800

    4

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-
-  // Show a message
   console.log('I am ready to show!');
+});
 
+$( document ).ready(function() {
+$(".dropdown-nav").click(function(){
+    $(this).children(".dropdown-content").slideToggle("200");
+  });
 });
